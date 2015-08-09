e742f9f5f1b1aa5508b298ea67b5b8197bd0bcd4
niuwanlu <niuwanlu@gmail.com>
2015-08-09 22:21:20 +0800

    add test for createMyOnlyIdealLove method in Singleton Pattern

--- spec/SingletonSpec.js
+++ spec/SingletonSpec.js
@@ -3,8 +3,15 @@ var singletonFile = require('../src/singletonPattern.js');
 describe("Singleton Pattern", function() {
 
     it('should get the instance', function() {
-        var singleton = new singletonFile.singleton();
+        var singleton = new singletonFile.singleton;
         expect(singleton.getMyOnlyIdealLove()).toBe("High Rich Handsome!");
     });
+
+    it('should be the same instance', function() {
+        var singletonLoveOne = new singletonFile.singleton;
+        var singletonLoveTwo = new singletonFile.singleton;
+        var isSame = (singletonLoveOne.getMyOnlyIdealLove() == singletonLoveTwo.getMyOnlyIdealLove());
+        expect(isSame).toBe(true);
+    })
 
 });
