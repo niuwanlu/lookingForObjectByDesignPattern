8370a56c1f015cd03a8366bd03743d15451e5806
niuwanlu <niuwanlu@gmail.com>
2015-08-24 21:07:41 +0800

    subject could remove observer

--- spec/ObserverSpec.js
+++ spec/ObserverSpec.js
@@ -7,6 +7,14 @@ describe('Observer Pattern', function() {
         var friend = new observer.friend();
         girl.addFriend(friend);
         expect(girl.friendsAmount()).toBe(1);
-    })
+    });
+
+    it('girl could remove friend', function () {
+        var girl = new observer.girl();
+        var friend = new observer.friend();
+        girl.addFriend(friend);
+        girl.removeFriend(friend);
+        expect(girl.friendsAmount()).toBe(0);
+    });
 
 });
--- src/observerPattern.js
+++ src/observerPattern.js
@@ -8,6 +8,13 @@ var girl = function() {
     this.addFriend = function(friend) {
         this.myFriends.push(friend);
     };
+
+    this.removeFriend = function(friend) {
+        var index = this.myFriends.indexOf(friend);
+        if(index > -1) {
+            this.myFriends.splice(index, 1);
+        }
+    };
 };
 
 var friend = function() {
